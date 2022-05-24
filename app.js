0e14aa9c18bdf2b1f35683bcce5d7785bdd335a1
gabsinator <gabsinator53@gmail.com>
2022-05-24 16:49:38 +0200

    bad commit xd

--- app.js
+++ app.js
@@ -26,8 +26,14 @@ window.onload = function() {
         
         var score = 0;
 
-        document.getElementById("game_over_screen").style = "null";
-        let score_counters = document.getElementsByClassName("score-counter");
+        //variables for html elements
+        const game_over_screen = document.getElementById("game_over_screen");
+        const score_counters = document.getElementsByClassName("score-counter");
+        const demo = document.getElementById("demo");
+        const viewCount_elem = document.getElementsByClassName("viewCount");
+        
+        game_over_screen.style = "null";
+        demo.innerHTML = "IN GAME";
 
         for (var i=0; i<score_counters.length; i++) {
             score_counters[i].innerHTML = `Score: ${score}`;
@@ -36,7 +42,7 @@ window.onload = function() {
         function getRandomInt(max) {
             return Math.floor(Math.random() * max);
         }
-
+        
         get_song = function(choice = String, callback) {
             fetch("songs.json")
                 .then(response => response.json())
@@ -66,7 +72,6 @@ window.onload = function() {
 
         }
         
-        document.getElementById("demo").innerHTML = "IN GAME";
 
 
         create_id = function(yt_url = String) {
@@ -151,13 +156,13 @@ window.onload = function() {
 
         show_views = () => {
             //show viewcount for both MVs
-            document.getElementsByClassName("viewCount")[0].innerHTML = `views: ${choice_1.views}`;
-            document.getElementsByClassName("viewCount")[1].innerHTML = `views: ${choice_2.views}`;
+            viewCount_elem[0].innerHTML = `views: ${choice_1.views}`;
+            viewCount_elem[1].innerHTML = `views: ${choice_2.views}`;
 
-            var choices = document.getElementsByClassName('viewCount');
+            //var choices = document.getElementsByClassName('viewCount');
 
-            for (var i=0; i<choices.length; i++) {
-                choices[i].style.visibility = 'visible';
+            for (var i=0; i<viewCount_elem.length; i++) {
+                viewCount_elem[i].style.visibility = 'visible';
             }
 
 
@@ -166,10 +171,8 @@ window.onload = function() {
         win = async (choice) => {
 
             console.log("W");
-            score++;
-            
-            let score_counters = document.getElementsByClassName("score-counter");
-
+            score++;     
+    
             for (var i=0; i<score_counters.length; i++) {
                 score_counters[i].innerHTML = `Score: ${score}`;
             }
@@ -260,7 +263,7 @@ window.onload = function() {
                     played = true;
 
                     let title = choice_div.getElementsByClassName("title")[0].innerHTML;
-                    document.getElementById("demo").innerHTML = title;
+                    demo.innerHTML = title;
 
                     show_views();
                     
